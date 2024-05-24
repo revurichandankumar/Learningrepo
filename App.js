6f170b1a6b0a5284949af3af636044a091e17b0a
Chandan.Revuri <chandan.revuri@onehubpos.com>
2024-05-24 07:13:31 +0530

    Epsiode 1

---
+++ App.js
@@ -0,0 +1,12 @@
+const parent=React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child1"},[
+    React.createElement("h1",{id:"heading1"},"heading1"),
+    React.createElement("h2",{id:"heading2"},"heading2")
+    ]),
+    React.createElement("div",{id:"child2"},[
+    React.createElement("h3",{id:"heading3"},"heading3"),
+    React.createElement("h4",{id:"heading4"},"heading4")
+    ]),
+]);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
